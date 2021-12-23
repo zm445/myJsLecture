30a3d05d9af25623a92b2fb805b4552f240c613f
zm445 <dlwnsgur303@naver.com>
2021-12-23 16:43:36 +0900

    update

---
+++ q058/player.js
@@ -0,0 +1,9 @@
+function Player(name, hp, attack) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.xp = 0;
+    this.info = function() {
+        return "["+ this.name + "(" + hp + "/" + this.hp + ") xp("+ this.xp + "/300)]\n";
+    }
+}
